f839a0702892450800eaf8fcf9fec5fadf9a98ab
stuartthomson <stuartwthomson@live.co.uk>
2019-12-01 18:01:42 +0000

    Day 1 Part 2, recursive boogaloo

--- 1/fuel-counter.hs
+++ 1/fuel-counter.hs
@@ -3,6 +3,15 @@
 fuel :: Int -> Int
 fuel x = (x `quot` 3) - 2
 
+-- For the Second part, we now need to take into account the weight of the fuel.
+-- The function signature does not change, we still are given an integer and want an integer back
+fuelPartTwo :: Int -> Int
+fuelPartTwo x
+    -- In the case that the fuel weight would be less than one, add nothing to the fuel
+    | fuel x < 1 = 0    
+    -- Otherwise, return the amount of fuel, plus the amount of fuel needed for the fuel!
+    | otherwise = (fuel x) + fuelPartTwo (fuel x)
+
 -- I will give the data as a text file, which one integer on each line.
 -- It will by default read in as an a character array, but I want a list of integers,
 -- so the type signature is:
@@ -18,4 +27,4 @@ main = do
     input <- readFile "input.txt"
     let inputInts = parseStr input
 
-    print (sum (map fuel inputInts))
+    print (sum (map fuelPartTwo inputInts))
